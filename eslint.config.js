428e38f28cb6b2aead4ab9a233677c34d987a809
pavanatti <pavanatti@simplesdental.com>
2026-03-31 12:05:43 -0300

    Add ESLint configuration and update Gruntfile for improved code quality

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+const neostandard = require('neostandard')
+
+module.exports = [
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      'examples/**',
+      'template/**',
+      'src/static/**',
+      'test-src/**'
+    ]
+  },
+  ...neostandard(),
+  {
+    languageOptions: {
+      parser: require('@babel/eslint-parser'),
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-env']
+        }
+      },
+      globals: {
+        env: 'readonly'
+      }
+    },
+    rules: {
+      'no-console': 'off',
+      'space-before-function-paren': 'off',
+      'import-x/no-unresolved': 'off'
+    }
+  }
+]
--- src/utils/lunrHelper.js
+++ src/utils/lunrHelper.js
@@ -1,94 +1,94 @@
-var lunr = require('lunr'),
-	cheerio = require('cheerio'),
-	template = require('./template'),
-	sanitizeHtml = require('sanitize-html'),
-	path = require('jsdoc/path'),
-	fs = require('jsdoc/fs'),
-	helper = require('jsdoc/util/templateHelper');
+const lunr = require('lunr')
+const cheerio = require('cheerio')
+const template = require('./template')
+const sanitizeHtml = require('sanitize-html')
+const path = require('jsdoc/path')
+const fs = require('jsdoc/fs')
+const helper = require('jsdoc/util/templateHelper')
 
-var store = exports.store = {};
-var index = exports.index = lunr(function(){
-	this.field('longname', {boost: 1000});
-	this.field('name', {boost: 500});
-	this.field('tags', {boost: 300});
-	this.field('kind', {boost: 110});
-	this.field('title', {boost: 100});
-	this.field('summary', {boost: 70});
-	this.field('description', {boost: 50});
-	this.field('body');
-	this.ref('id');
-});
+const store = exports.store = {}
+const index = exports.index = lunr(function () {
+  this.field('longname', { boost: 1000 })
+  this.field('name', { boost: 500 })
+  this.field('tags', { boost: 300 })
+  this.field('kind', { boost: 110 })
+  this.field('title', { boost: 100 })
+  this.field('summary', { boost: 70 })
+  this.field('description', { boost: 50 })
+  this.field('body')
+  this.ref('id')
+})
 
-var sanitize = function(html){
-	if (typeof html !== 'string') return void 0;
-	return sanitizeHtml(html, {allowedTags: [], allowedAttributes: []}).replace(/\s+/g, ' ').trim();
-};
+const sanitize = function (html) {
+  if (typeof html !== 'string') return undefined
+  return sanitizeHtml(html, { allowedTags: [], allowedAttributes: [] }).replace(/\s+/g, ' ').trim()
+}
 
-var _variations = function(parts){
-	var result = [];
-	result.push(parts.join('.'));
-	if (parts.length > 1){
-		parts.shift();
-		result.push(_variations(parts));
-	} else if (parts.length === 1){
-		var instance = parts[0].indexOf('#');
-		if (instance !== -1){
-			result.push(parts[0].substr(instance + 1));
-		}
-	}
-	return result.join(' ');
-};
+const _variations = function (parts) {
+  const result = []
+  result.push(parts.join('.'))
+  if (parts.length > 1) {
+    parts.shift()
+    result.push(_variations(parts))
+  } else if (parts.length === 1) {
+    const instance = parts[0].indexOf('#')
+    if (instance !== -1) {
+      result.push(parts[0].substr(instance + 1))
+    }
+  }
+  return result.join(' ')
+}
 
-var variations = function(longname){
-	if (typeof longname != 'string' || longname === '') return '';
-	return _variations(longname.split('.'));
-};
+const variations = function (longname) {
+  if (typeof longname !== 'string' || longname === '') return ''
+  return _variations(longname.split('.'))
+}
 
-var tags = function(doclet){
-	var result = [];
-	if (doclet.longname && doclet.longname !== ''){
-		result.push(variations(doclet.longname));
-	}
-	return result.join(' ');
-};
+const tags = function (doclet) {
+  const result = []
+  if (doclet.longname && doclet.longname !== '') {
+    result.push(variations(doclet.longname))
+  }
+  return result.join(' ')
+}
 
-var parseBody = function(html){
-	if (!html || typeof html !== 'string') return;
-	var $ = cheerio.load(html);
-	return $('#main').text().replace(/\s+/g, ' ').trim();
-};
+const parseBody = function (html) {
+  if (!html || typeof html !== 'string') return
+  const $ = cheerio.load(html)
+  return $('#main').text().replace(/\s+/g, ' ').trim()
+}
 
-var add = exports.add = function(doclet, html){
-	var id = helper.longnameToUrl[doclet.longname];
-	index.add(store[id] = {
-		"id": id,
-		"kind": doclet.kind,
-		"title": doclet.pageTitle,
-		"longname": doclet.longname,
-		"name": doclet.name,
-		"tags": tags(doclet),
-		"summary": sanitize(helper.resolveLinks(doclet.summary)),
-		"description": sanitize(helper.resolveLinks(doclet.classdesc || doclet.description)),
-		"body": parseBody(html)
-	});
-	if (doclet.kind === 'class' || doclet.kind === 'namespace'){
-		doclet.symbols.member.forEach(function(member){
-			add(member);
-		});
-		doclet.symbols.function.forEach(function(fn){
-			add(fn);
-		});
-		doclet.symbols.typedef.forEach(function(typedef){
-			add(typedef);
-		});
-	}
-};
+const add = exports.add = function (doclet, html) {
+  const id = helper.longnameToUrl[doclet.longname]
+  index.add(store[id] = {
+    id,
+    kind: doclet.kind,
+    title: doclet.pageTitle,
+    longname: doclet.longname,
+    name: doclet.name,
+    tags: tags(doclet),
+    summary: sanitize(helper.resolveLinks(doclet.summary)),
+    description: sanitize(helper.resolveLinks(doclet.classdesc || doclet.description)),
+    body: parseBody(html)
+  })
+  if (doclet.kind === 'class' || doclet.kind === 'namespace') {
+    doclet.symbols.member.forEach(function (member) {
+      add(member)
+    })
+    doclet.symbols.function.forEach(function (fn) {
+      add(fn)
+    })
+    doclet.symbols.typedef.forEach(function (typedef) {
+      add(typedef)
+    })
+  }
+}
 
-exports.writeFilesSync = function(pretty){
-	var jsonFile = path.join(template.config.dir.output, 'js/lunr-data.json'),
-		dataFile = path.join(template.config.dir.output, 'js/lunr-data.js'),
-		data = {index: index, store: store},
-		json = pretty ? JSON.stringify(data, null, 2) : JSON.stringify(data);
-	fs.writeFileSync(jsonFile, json, "utf8");
-	fs.writeFileSync(dataFile, "window.lunrData = " + json + ";", "utf8");
-};
+exports.writeFilesSync = function (pretty) {
+  const jsonFile = path.join(template.config.dir.output, 'js/lunr-data.json')
+  const dataFile = path.join(template.config.dir.output, 'js/lunr-data.js')
+  const data = { index, store }
+  const json = pretty ? JSON.stringify(data, null, 2) : JSON.stringify(data)
+  fs.writeFileSync(jsonFile, json, 'utf8')
+  fs.writeFileSync(dataFile, 'window.lunrData = ' + json + ';', 'utf8')
+}
